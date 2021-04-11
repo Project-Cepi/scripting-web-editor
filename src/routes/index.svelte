03b1d4c0290ea03728e9a247160450536fcdd843
LeoDog896 <LeoDog896@hotmail.com>
2021-04-11 19:03:34 -0400

    Add tab bindings

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,18 +2,23 @@
 	export const ssr = false;
 </script>
 <script lang="ts">
-	import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
+	import { EditorState, basicSetup } from "@codemirror/basic-setup"
+	import { EditorView, keymap } from "@codemirror/view"
+	import { defaultTabBinding } from "@codemirror/commands"
 
 	let editor = new EditorView({
 		state: EditorState.create({
-		extensions: [basicSetup]
-	}),
+			extensions: [
+				basicSetup,
+				keymap.of([defaultTabBinding]),
+			],
+		}),
 		parent: document.body
 	})
 </script>
 
 <style lang="scss">
-	
+
 </style>
 
 <main>
