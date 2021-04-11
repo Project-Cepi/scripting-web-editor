9c65cf1d347fa7144fabf37a37ecc731c0fe0fd1
LeoDog896 <LeoDog896@hotmail.com>
2021-04-11 18:57:38 -0400

    Add codemirror

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,20 @@
-<script lang="ts">
-	
+<script context="module">
+	export const ssr = false;
 </script>
+<script lang="ts">
+	import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
+
+	let editor = new EditorView({
+		state: EditorState.create({
+		extensions: [basicSetup]
+	}),
+		parent: document.body
+	})
+</script>
+
+<style lang="scss">
+	
+</style>
 
 <main>
 
