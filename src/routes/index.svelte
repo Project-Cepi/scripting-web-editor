65d5ea478c124d729d5e93148848cb59e90695bf
LeoDog896 <LeoDog896@hotmail.com>
2021-04-12 13:48:23 -0400

    Base horrible web design

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,47 @@
-<script context="module">
-	export const ssr = false;
-</script>
-<script lang="ts">
-	import Editor from '$lib/editor.svelte'
-</script>
-
+<main>
+	<div id="info">
+		<h1>Luae Web Editor</h1>
+		<p>Cepi's scripting engine's editor.</p>
+	</div>
+	<div id="start">
+		<button>Create New Script</button>
+	</div>
+</main>
 <style lang="scss">
 
-</style>
+	main {
+		position: absolute;
+		transform: translateY(-50%);
+		width: 100%;
+		top: 50%;
+		text-align: center;
+		display: flex;
+		justify-content: space-evenly;
+	}
 
-<main>
-	<Editor/>
-</main>
+	#start {
+		display: flex;
+  		align-items: center;
+	}
+
+	h1, p {
+		color: white;
+	}
+
+	h1 {
+		font-size: 5em;
+	}
+
+	p {
+		font-size: 3em;
+	}
+
+	button {
+		padding: 50px;
+		font-size: 2em;
+		border-radius: 10px;
+		background: rgba(0, 0, 0, 0);
+		border: white solid 2px;
+		color: white;
+	}
+</style>
