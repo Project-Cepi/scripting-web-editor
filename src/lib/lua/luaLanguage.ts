bee4154f52a5708cd6dda4488b64edda2e328a28
LeoDog896 <LeoDog896@hotmail.com>
2021-04-28 18:07:55 -0400

    Nice syntax highlighting

--- src/lib/lua/luaLanguage.ts
+++ src/lib/lua/luaLanguage.ts
@@ -1,8 +1,27 @@
-import { LezerLanguage, LanguageSupport } from "@codemirror/language"
+import { LezerLanguage, LanguageSupport, foldNodeProp, foldInside, indentNodeProp } from "@codemirror/language"
 import { parser } from "./lua.js"
+import { styleTags, tags } from "@codemirror/highlight"
+import { completeFromList } from "@codemirror/autocomplete"
 
 const parserWithMetadata = parser.configure({
-	props: []
+	props: [
+		styleTags({
+			Number: tags.number, 
+			CompareOp: tags.compareOperator,
+			Comment: tags.comment,
+			Identifier: tags.variableName,
+			SingleString: tags.string,
+			"( )": tags.paren,
+			"[ ]": tags.squareBracket,
+			"{ }": tags.brace
+		}),
+		indentNodeProp.add({
+			Application: context => context.column(context.node.from) + context.unit
+		}),
+		foldNodeProp.add({
+			Application: foldInside
+		})
+	]
 })
 
 export const luaLanguage = LezerLanguage.define({
@@ -12,6 +31,18 @@ export const luaLanguage = LezerLanguage.define({
 	}
 })
 
+export const luaCompletion = luaLanguage.data.of({
+	autocomplete: completeFromList([
+		{ label: "defun", type: "keyword" },
+		{ label: "defvar", type: "keyword" },
+		{ label: "let", type: "keyword" },
+		{ label: "cons", type: "function" },
+		{ label: "car", type: "function" },
+		{ label: "cdr", type: "function" }
+	])
+})
+
+
 export function lua(): LanguageSupport {
-	return new LanguageSupport(luaLanguage)
-  }
+	return new LanguageSupport(luaLanguage, [luaCompletion])
+}
